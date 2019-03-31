7e3bfd6f446524222acf85b26fc2525d96d7b2ac
DexiGG <invokedex@gmail.com>
2019-03-31 20:37:41 +0600

    WorldofTancs

---
+++ Day7 (Tanks)/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLib.WorldOfTanks;
+
+namespace Day7__Tanks_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int COUNT_OF_TANKS = 5;
+            const int END_RANGE = 100;
+            const string FIRST_KIND_OF_TANK = "T-34";
+            const string SECOND_KIND_OF_TANK = "Pantera";
+            int weapon;
+            int armourLevel;
+            int manoeuvrability;
+
+            Random rnd = new Random();
+            Tank[] t34 = new Tank[COUNT_OF_TANKS];
+            Tank[] pantera = new Tank[COUNT_OF_TANKS];
+
+            for (int i = 0; i < COUNT_OF_TANKS; i++)
+            {
+                weapon = rnd.Next(END_RANGE);
+                armourLevel = rnd.Next(END_RANGE);
+                manoeuvrability = rnd.Next(END_RANGE);
+                t34[i] = new Tank(FIRST_KIND_OF_TANK, weapon, armourLevel, manoeuvrability);
+
+                weapon = rnd.Next(END_RANGE);
+                armourLevel = rnd.Next(END_RANGE);
+                manoeuvrability = rnd.Next(END_RANGE);
+                pantera[i] = new Tank(SECOND_KIND_OF_TANK, weapon, armourLevel, manoeuvrability);
+            }
+
+            Battle(t34, pantera, COUNT_OF_TANKS);
+        }
+        static void Battle(Tank[] t34, Tank[] pantera, int COUNT_OF_TANKS)
+        {
+            for (int i = 0; i < COUNT_OF_TANKS; i++)
+            {
+                Console.WriteLine("Пара №{0}", i + 1);
+                Tank.Show(t34[i], pantera[i]);
+                try
+                {
+                    if (t34[i] ^ pantera[i]) Console.WriteLine("Победитель сражения: T-34\n");
+                    else Console.WriteLine("Победитель сражения: Pantera\n");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ MyClassLib/WorldOfTanks/Tank.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib.WorldOfTanks
+{
+    public class Tank
+    {
+        public string Name { get; set; }
+        public int Weapon { get; set; }
+        public int ArmourLevel { get; set; }
+        public int Manoeuvrability { get; set; }
+
+        #region Конструкторы
+        public Tank() {}
+        public Tank(string name, int weapon, int armourLevel, int manoeuvrability)
+        {
+            Name = name;
+            Weapon = weapon; ArmourLevel = armourLevel; Manoeuvrability = manoeuvrability;
+        }
+        #endregion
+
+        #region Вывод на консоль
+        public static void Show(Tank tank_1, Tank tank_2)
+        {
+            Console.WriteLine("Название танка:\t\t\t" + tank_1.Name + "\t\t" + tank_2.Name +
+                "\nУровень боекомплекта:\t\t" + tank_1.Weapon + "\t\t" + tank_2.Weapon +
+                "\nУровень брони:\t\t\t" + tank_1.ArmourLevel + "\t\t" + tank_2.ArmourLevel +
+                "\nУровень маневренности:\t\t" + tank_1.Manoeuvrability + "\t\t" + tank_2.Manoeuvrability);
+        }
+        #endregion
+
+        #region Перегрузка оператора
+        public static bool operator ^(Tank tank_1, Tank tank_2)
+        {
+            int targetRatio = 2;
+            int supremacyRatio = 0, identicalRatio = 0;
+
+            if (tank_1.Weapon > tank_2.Weapon) supremacyRatio++;
+            else if (tank_1.Weapon == tank_2.Weapon) identicalRatio++;
+            if (tank_1.ArmourLevel > tank_2.ArmourLevel) supremacyRatio++;
+            else if (tank_1.ArmourLevel == tank_2.ArmourLevel) identicalRatio++;
+            if (tank_1.Manoeuvrability > tank_2.Manoeuvrability) supremacyRatio++;
+            else if (tank_1.Manoeuvrability == tank_2.Manoeuvrability) identicalRatio++;
+
+            if (identicalRatio >= targetRatio || (identicalRatio == targetRatio - 1 && supremacyRatio == targetRatio - 1))
+                throw new Exception("Победитель сражения: Неизвестен\n");
+
+            return supremacyRatio >= targetRatio;
+        }
+        #endregion
+    }
+}
